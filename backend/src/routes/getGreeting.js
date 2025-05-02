18b691eace254b31f77e5998d0c08f71c207d57c
John Morris <johnpmorris@gmail.com>
2025-05-02 19:55:42 -0400

    changed greetings.  following the instructions

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,17 @@
-const GREETING = 'Hello world!';
+const GREETINGS = [
+    'Hello world!',
+    'Ahoy, matey!',
+    'Greetings, Earthling!',
+    'Howdy, partner!',
+    'Salutations!',
+    'Bonjour, mon ami!',
+    'Hola, amigo!',
+    'Ciao, amico!',
+    'Konnichiwa, tomodachi!',
+];
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        greeting: GREETINGS[Math.floor(Math.random() * GREETINGS.length)],
     });
 };
